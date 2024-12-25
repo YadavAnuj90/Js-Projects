de08efb18cf1dd2325d3634f2d78ad2714c62dcd
Anuj Kumar <anujy5706@gmail.com>
2024-12-25 12:31:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const checkBoxList = document.querySelectorAll('.custom-cheackbox');
+const inputFields = document.querySelectorAll('.goal-input');
+const errorLavel = document.querySelector('.error-label')
+const progressBar = document.querySelector('.progress-bar');
+const progressValue = document.querySelector('.progress-value');
+
+checkBoxList.forEach((checkbox) => {
+    checkbox.addEventListener('click' , (e) => {
+         const allFieldsFilled = [...inputFields].every((input) => {
+            return input.value
+         })
+         if (allFieldsFilled) {
+             
+            checkbox.parentElement.classList.toggle('completed');
+            progressValue.style.width = '33.33%'
+         } else {
+
+           progressBar.classList.add('show-error');
+         }
+    })
+})
+
+inputFields.forEach((input) => {
+    input.addEventListener('focus' , () => {
+        progressBar.classList.remove('show-error');
+    })
+})
+  
